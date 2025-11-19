16602fd05d28e4d22423251d5702a51f9a6eb886
Dimas Bagas Baskoro <deniasitubagas@gmail.com>
2025-11-19 13:52:58 +0700

    feat(ProfilSoalResource): add create action to header actions
    Add CreateAction to header actions in ListProfilSoals page to allow creating new ProfilSoal records directly from the list view

--- app/Filament/Resources/ProfilSoalResource/Pages/ListProfilSoals.php
+++ app/Filament/Resources/ProfilSoalResource/Pages/ListProfilSoals.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\ProfilSoalResource\Pages;
 
 use App\Filament\Resources\ProfilSoalResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProfilSoals extends ListRecords
 {
     protected static string $resource = ProfilSoalResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
